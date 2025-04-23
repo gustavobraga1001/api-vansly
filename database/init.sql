1997d7033bfcdb06ededa65e019b4e182e38bfb4
Gustavo Braga <gustavobraga@MacBook-Air-de-Gustavo.local>
2025-04-22 21:48:52 -0300

    update readme and add script sql

---
+++ database/init.sql
@@ -0,0 +1,2 @@
+INSERT INTO "users" (id, name, email, password_hash) VALUES
+('f185999b-d2bc-469a-8275-263e430bb67f','João', 'joao@email.com', '$2a$06$K42VPPNia63NwtVc6PvAZuwbx5BL3xOI0Gjs0rbaS.18FpgmbEara')
