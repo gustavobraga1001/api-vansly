74ee0ddaf4b5f20a3ad4e0c87c113e2b4babe8f5
gustavobraga-sigma <gustavo.braga@smartsigma.com.br>
2025-05-29 10:37:50 -0300

    Fix: delete files

--- src/use-cases/factories/make-alter-password-use-case.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { AlterPasswordUseCase } from '../alter-password'
-
-export function makeAlterPasswordUseCase() {
-  const usersRepository = new PrismaUsersRepository()
-  const alterPasswordUseCase = new AlterPasswordUseCase(usersRepository)
-
-  return alterPasswordUseCase
-}
