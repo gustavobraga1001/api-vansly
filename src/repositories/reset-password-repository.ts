4a04fbb5b0b04956f9dd56c3f88e931232a73088
gustavobraga-sigma <gustavo.braga@smartsigma.com.br>
2025-05-29 10:27:41 -0300

    Fix: migration delete_reset_password

--- src/repositories/reset-password-repository.ts
+++
@@ -1,9 +0,0 @@
-import { PasswordReset, Prisma } from '@prisma/client'
-
-export interface ResetPasswordRepository {
-  create(data: Prisma.PasswordResetCreateInput): Promise<PasswordReset>
-  findByEmailAndCode(
-    userId: string,
-    code: string,
-  ): Promise<PasswordReset | null>
-}
